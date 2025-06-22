b815888cfe6f7700039d0e45c7622cd6a20f4154
Christoph Schufer <christoph.schufer@hotmail.com>
2025-06-22 19:25:14 +0200

    implemented CRUD for invoices

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,8 +1,13 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Sevillana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
 export const lusitana = Lusitana({
   weight: ['400', '700'], // Regular and Bold
+  subsets: ['latin'], // Ensure the font is loaded with the Latin subset
+});
+
+export const sevillana = Sevillana({
+  weight: ['400'], // Regular
   subsets: ['latin'], // Ensure the font is loaded with the Latin subset
 });
